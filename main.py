a9b834268b3c42585eaf914d731c1cba67b9c8ed
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2026-01-16 09:20:46 +0900

    feat: comments 도메인 라우터 분리

--- main.py
+++ main.py
@@ -1,46 +1,18 @@
 from fastapi import FastAPI, Depends
 
-from routers import users, posts
+from routers import users, posts, comments
 from schemas.commons import PostId, CommentId, Page
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(posts.router)
+app.include_router(comments.router)
 
 
 
 
 
-# ----- COMMENTS ----- #
 
-# List all comments for a post
-@app.get("/posts/{post_id}/comments")
-async def get_comments(post_id: PostId, page: Page):
-    pass
-
-
-# post comment
-@app.post("/posts/{post_id}/comments")
-async def post_comment(post_id: PostId):
-    pass
-
-
-# edit comment
-@app.patch("/posts/{post_id}/comments/{comment_id}")
-async def update_comment(post_id: PostId, comment_id: CommentId):
-    pass
-
-
-# delete comment
-@app.delete("/posts/{post_id}/comments/{comment_id}")
-async def delete_comment(post_id: PostId, comment_id: CommentId):
-    pass
-
-
-# comment list I wrote
-@app.get("/comments/me")
-async def get_comments_mine(page: Page):
-    pass
 
 
 # ----- LIKES ----- #
