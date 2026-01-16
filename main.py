e39460a526785329dea1c80e4299e48b63c2deb5
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2026-01-16 09:23:31 +0900

    feat: likes 도메인 라우터 분리

--- main.py
+++ main.py
@@ -1,41 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 
-from routers import users, posts, comments
-from schemas.commons import PostId, CommentId, Page
+from routers import users, posts, comments, likes
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(posts.router)
 app.include_router(comments.router)
-
-
-
-
-
-
-
-
-# ----- LIKES ----- #
-
-# register like
-@app.post("/posts/{post_id}/likes")
-async def post_like(post_id: PostId):
-    pass
-
-
-# delete like
-@app.delete("/posts/{post_id}/likes")
-async def delete_like(post_id: PostId):
-    pass
-
-
-# check like status
-@app.get("/posts/{post_id}/likes")
-async def get_likes_status(post_id: PostId):
-    pass
-
-
-# post list I liked
-@app.get("/posts/liked")
-async def get_posts_liked(page: Page):
-    pass
+app.include_router(likes.router)
---
+++ routers/likes.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+
+from schemas.commons import PostId, Page
+
+router = APIRouter()
+
+
+# register like
+@router.post("/posts/{post_id}/likes")
+async def post_like(post_id: PostId):
+    return {"success": "post_like"}
+
+
+# delete like
+@router.delete("/posts/{post_id}/likes")
+async def delete_like(post_id: PostId):
+    return {"success": "delete_like"}
+
+
+# check like status
+@router.get("/posts/{post_id}/likes")
+async def get_likes_status(post_id: PostId):
+    return {"success": "get_likes_status"}
+
+
+# post list I liked
+@router.get("/posts/liked")
+async def get_posts_liked(page: Page):
+    return {"success": "get_posts_liked"}
