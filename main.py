7720b88dc9d071bd2765b7216b3d2441b5ff10b5
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2026-01-17 09:42:53 +0900

    chore: dotenv 의존성 추가 및 위치 라우터 위로 변경

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from routers import users, posts, comments, likes
 from dotenv import load_dotenv
 
 load_dotenv()
+
+from routers import users, posts, comments, likes
 
 app = FastAPI()
 app.include_router(users.router)
