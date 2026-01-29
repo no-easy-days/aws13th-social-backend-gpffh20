80dd2b48a1fb28204fd8c75f04fa66f0d37e3229
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2026-01-29 12:26:17 +0900

    refactor: User에 Post 양방향 설정 필요

--- db/models/user.py
+++ db/models/user.py
@@ -1,9 +1,14 @@
 from datetime import datetime
 
 from sqlalchemy import String, DateTime, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db.base import Base
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from db.models.post import Post
 
 class User(Base):
     __tablename__ = "users"
@@ -14,3 +19,5 @@ class User(Base):
     nickname: Mapped[str] = mapped_column(String(15), nullable=False)
     profile_img: Mapped[str | None] = mapped_column(String(500))
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+
+    posts: Mapped[list[Post]] = relationship("Post", back_populates="author")
